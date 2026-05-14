14a9fd97aa2bf124cb81789f9b47956261963962
Nikag0 <Serito2@mail.ru>
2026-05-14 14:58:45 +0300

    Доработка ComponentViewModel

--- WMS.Desktop/ViewModels/ComponentsViewModel.cs
+++ WMS.Desktop/ViewModels/ComponentsViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 using WMS.Application.Abstractions;
 using WMS.Application.Services;
@@ -18,7 +19,7 @@ namespace WMS.Desktop.ViewModels
 {
     public class ComponentsViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<ComponentDTO> FilteredComponents { get; } = new();
+        public ICollectionView ComponentsView { get; }
 
         public string NewArticle { get; set; }
         public string NewName { get; set; }
@@ -30,7 +31,7 @@ namespace WMS.Desktop.ViewModels
             {
                 _searchText = value;
                 OnPropertyChanged();
-                ApplyFilter();
+                ComponentsView.Refresh();
             }
         }
         public bool IsAdding
@@ -71,9 +72,24 @@ namespace WMS.Desktop.ViewModels
             AddModeOnCommand = new RelayCommand(_ =>{IsAdding = true;});
             AddModeOffCommand = new RelayCommand(_ =>{IsAdding = false;});
 
-            _dataStore.Components.CollectionChanged += OnComponentsCacheChanged;
+            ComponentsView = CollectionViewSource.GetDefaultView(_dataStore.Components);
+
+            ComponentsView.Filter = FilterComponent;
 
             _ = InitializeDataAsync();
+        }
+
+        private bool FilterComponent(object obj)
+        {
+            if (obj is not ComponentDTO component) return false;
+
+            if (string.IsNullOrWhiteSpace(SearchText)) return true;
+
+            var text = SearchText.ToLower();
+
+            return (component.Article != null && component.Article.ToLower().Contains(text)) ||
+                   (component.Name != null && component.Name.ToLower().Contains(text)) ||
+                   (component.Manufacturer != null && component.Manufacturer.ToLower().Contains(text));
         }
 
         private async Task InitializeDataAsync()
@@ -82,29 +98,13 @@ namespace WMS.Desktop.ViewModels
             {
                 await RefreshAsync();
             }
-            else
-            {
-                ApplyFilter();
-            }
-        }
-
-        private void OnComponentsCacheChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            ApplyFilter();
         }
 
         public async Task RefreshAsync()
         {
-
             try
             {
                 await _dataStore.RefreshComponentsAsync();
-
-                ApplyFilter();
-            }
-            catch (OverallDomainException ex)
-            {
-                _dialogService.ShowWarning(ex.Message);
             }
             catch (Exception ex)
             {
@@ -138,28 +138,28 @@ namespace WMS.Desktop.ViewModels
             }
         }
 
-        private void ApplyFilter()
-        {
-            var query = _dataStore.Components.AsEnumerable();
+        //private void ApplyFilter()
+        //{
+        //    var query = _dataStore.Components.AsEnumerable();
 
-            if (!string.IsNullOrWhiteSpace(SearchText))
-            {
-                var text = SearchText.ToLower();
+        //    if (!string.IsNullOrWhiteSpace(SearchText))
+        //    {
+        //        var text = SearchText.ToLower();
 
-                query = query.Where(c =>
-                        c.Article != null && c.Article.ToLower().Contains(text) ||
-                        c.Name != null && c.Name.ToLower().Contains(text) ||
-                        c.Manufacturer != null && c.Manufacturer.ToLower().Contains(text));
-            }
+        //        query = query.Where(c =>
+        //                c.Article != null && c.Article.ToLower().Contains(text) ||
+        //                c.Name != null && c.Name.ToLower().Contains(text) ||
+        //                c.Manufacturer != null && c.Manufacturer.ToLower().Contains(text));
+        //    }
 
-            var resultList = query.ToList();
+        //    var resultList = query.ToList();
 
-            FilteredComponents.Clear();
+        //    FilteredComponents.Clear();
 
-            foreach (var item in resultList) FilteredComponents.Add(item);
+        //    foreach (var item in resultList) FilteredComponents.Add(item);
 
-            OnPropertyChanged(nameof(FilteredComponents));
-        }
+        //    OnPropertyChanged(nameof(FilteredComponents));
+        //}
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
